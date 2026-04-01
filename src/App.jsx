d64b94397aa0641e00d3cac9f8369ad78a8a0491
tosan1470 <macaulaytesolatosan@gmail.com>
2026-03-31 21:10:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 }
 
 const handleDeposit = async () => {
-  const response = await fetch("http://localhost:3000/create-checkout-session", {
+  const response = await fetch("http://thecuearena-backend.onrender.com/create-checkout-session", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
